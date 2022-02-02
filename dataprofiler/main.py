4bdda0ac662e3acd196bad387b0d2801b343dd9e
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-02-02 08:00:29 -0800

    add new params

--- dataprofiler/main.py
+++ dataprofiler/main.py
@@ -1,6 +1,9 @@
 import argparse
 import logging
 
+from dataprofiler.storage.LocalStorageEngine import LocalStorageEngine
+from dataprofiler import *
+
 
 def main():
     log = logging.getLogger(__name__)
@@ -10,18 +13,39 @@ def main():
     handler.setFormatter(formatter)
     log.addHandler(handler)
     args_parser = argparse.ArgumentParser(description='Generate data profiling report')
+    args_parser.add_argument("--storage_type",
+                             metavar="storage_type",
+                             choices=['local', 's3'],
+                             required=True,
+                             help="The storage type that you want to connect to. Possible values: "
+                                  "'local', 's3'"
+                             )
     args_parser.add_argument("--input_file",
                              metavar="full_file_path",
                              required=True,
                              help="Full file path of the input file which you want to profile")
+    args_parser.add_argument("--file_format",
+                             metavar="full_file_path",
+                             choices=['parquet', 'csv', 'json'],
+                             required=True,
+                             help="File format of the input file which you want to profile, Possible values: csv ,json")
     args_parser.add_argument("--report_path",
                              metavar="report_output_path",
                              default=None,
                              help="Parent dir for storing output report")
     args_parser.add_argument("--report_format",
                              metavar="storage_type",
-                             choices=['local', 's3', 'hdfs'],
-                             required=True,
-                             help="The storage type that you want to connect to. Possible values: "
-                                  "'local', 's3', 'hdfs'"
+                             choices=['html', 'json'],
+                             help="The output report format Possible values: 'html', 'json' "
                              )
+    args_parser.add_argument("--separator",
+                             metavar="csv_file_separator",
+                             default=None,
+                             help="Separator for csv file")
+    args_parser.add_argument("--na_val",
+                             metavar="csv_file_na",
+                             default=None,
+                             help="Null value identifier for csv file")
+    args = args_parser.parse_args()
+    if args.storage_type == "local":
+        storage_engine = LocalStorageEngine()
