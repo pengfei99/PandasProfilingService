f43861f67a58241ecda23f202858aeff8b1674b2
pengfei <liu.pengfei@hotmail.fr>
2022-02-07 16:17:18 +0100

    update shell for running with minimal action

--- main.sh
+++ main.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ "$FILE_FORMAT" = "parquet" ];
-then python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format parquet --report_render_path /html --report_format html --report_name index
+then python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format parquet --report_render_path /html --report_format html --report_name index --minimal $MINIMAL --explorative $EXPLORATIVE
 elif [ "$FILE_FORMAT" = "json" ];
-then python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format json --report_render_path /html --report_format html --report_name index
+then python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format json --report_render_path /html --report_format html --report_name index --minimal $MINIMAL --explorative $EXPLORATIVE
 elif [ "$FILE_FORMAT" = "csv" ];
 then
     if [ -z "$NA_VAL" ];
-    then python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format $FILE_FORMAT --separator $SEPARATOR --report_render_path /html --report_format html --report_name index
-    else python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format $FILE_FORMAT --separator $SEPARATOR --na_val $NA_VAL --report_render_path /html --report_format html --report_name index
+    then python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format $FILE_FORMAT --separator $SEPARATOR --report_render_path /html --report_format html --report_name index --minimal $MINIMAL --explorative $EXPLORATIVE
+    else python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format $FILE_FORMAT --separator $SEPARATOR --na_val $NA_VAL --report_render_path /html --report_format html --report_name index --minimal $MINIMAL --explorative $EXPLORATIVE
     fi;
 fi;
