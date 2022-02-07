88ba59bc0fe65481ac0dd0f7aba03ec5aab0000a
pengfei <liu.pengfei@hotmail.fr>
2022-02-07 13:37:52 +0100

    change format bash to sh

--- main.sh
+++ main.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ "$FILE_FORMAT"=="parquet" ]];
+if [ "$FILE_FORMAT" = "parquet" ];
 then echo python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format parquet --report_render_path /html --report_format html --report_name index
-elif [[ "$FILE_FORMAT"=="json" ]];
+elif [ "$FILE_FORMAT" = "json" ];
 then echo python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format json --report_render_path /html --report_format html --report_name index
-elif [[ "$FILE_FORMAT"=="csv" ]];
+elif [ "$FILE_FORMAT" = "csv" ];
 then
-    if [[ -z "$NA_VAL" ]];
+    if [ -z "$NA_VAL" ];
     then echo python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format $FILE_FORMAT --separator $SEPARATOR --report_render_path /html --report_format html --report_name index
     else echo python dataprofiler/main.py --action render --storage_type s3 --input_file $INPUT_FILE --file_format $FILE_FORMAT --separator $SEPARATOR --na_val $NA_VAL --report_render_path /html --report_format html --report_name index
     fi;
